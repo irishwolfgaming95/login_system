99bc48a4b56ed03b4de96f5740e98a2e6f67a527
irishwolfgaming95 <valkyire8@gmail.com>
2020-12-14 19:09:12 -0600

    added a create-user DTO and database migration

--- login-system/src/user/user.controller.ts
+++ login-system/src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/items/dto/create-user.dto';
 
 @Controller('user')
 export class UserController {}
