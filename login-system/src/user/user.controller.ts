444ef2cbd95e0127d9967fda2c9b8f1040019421
irishwolfgaming95 <valkyire8@gmail.com>
2020-12-17 16:19:16 -0600

    building the user controller

--- login-system/src/user/user.controller.ts
+++ login-system/src/user/user.controller.ts
@@ -10,4 +10,19 @@ import {
 import { CreateUserDto } from 'src/items/dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get()
+  findAll() {
+    return 'get all users';
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateUserDto, @Param('id') id): string {
+    return `Update ${id}-Username: ${updateItemDto.username}`;
+  }
+}
